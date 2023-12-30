5d741c0c78c70051a02e91643b8b49abcef37721
zipypie <201980170@psu.palawan.edu.ph>
2023-12-30 10:40:34 +0800

    some minor changes

--- getPopularMovies.js
+++ getPopularMovies.js
@@ -32,52 +32,58 @@ document.addEventListener("DOMContentLoaded", function () {
     const popularMoviesToDisplay = movies.slice(0, 16);
 
     popularMoviesToDisplay.forEach(movie => {
-      const movieCard = document.createElement("div");
-      movieCard.classList.add("movie-card");
-      movieCard.dataset.movieId = movie.id; // Set dataset attribute for movie ID
-
-      const imgDiv = document.createElement("div");
-      imgDiv.classList.add("img-div");
-
-      const img = document.createElement("img");
-      img.src = `http://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-      img.alt = movie.title;
-      img.id = "movie-img";
-
-      const movieInfoContainer = document.createElement("div");
-      movieInfoContainer.classList.add("movie-info-container");
-
-      const title = document.createElement("h2");
-      title.classList.add("f-s-mi");
-      title.textContent = movie.title;
-      title.id = "movie-title";
-
-      const releaseDate = document.createElement("span");
-      releaseDate.classList.add("date-release", "f-s-mi");
-      releaseDate.textContent = `Release Date: ${movie.release_date}`;
-      releaseDate.id = "movie-release-date";
-
-      const genre = document.createElement("span");
-      genre.classList.add("genre", "f-s-mi");
-
-      if (Array.isArray(movie.genres)) {
-        genre.textContent = "Genres: " + movie.genres.map(genre => genre.name).join(", ");
-      } else {
-        genre.textContent = "Genres: N/A";
-      }
-
-      genre.id = "movie-genre";
-
-      imgDiv.appendChild(img);
-      movieInfoContainer.appendChild(title);
-      movieInfoContainer.appendChild(releaseDate);
-      movieInfoContainer.appendChild(genre);
-
-      movieCard.appendChild(imgDiv);
-      movieCard.appendChild(movieInfoContainer);
-
+      const movieCard = createMovieCard(movie);
       movieContainer.appendChild(movieCard);
     });
+  }
+
+  // Function to create a movie card
+  function createMovieCard(movie) {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+    movieCard.dataset.movieId = movie.id; // Set dataset attribute for movie ID
+
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add("img-div");
+
+    const img = document.createElement("img");
+    img.src = `http://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+    img.alt = movie.title;
+    img.id = "movie-img";
+
+    const movieInfoContainer = document.createElement("div");
+    movieInfoContainer.classList.add("movie-info-container");
+
+    const title = document.createElement("h2");
+    title.classList.add("f-s-mi");
+    title.textContent = movie.title;
+    title.id = "movie-title";
+
+    const releaseDate = document.createElement("span");
+    releaseDate.classList.add("date-release", "f-s-mi");
+    releaseDate.textContent = `Release Date: ${movie.release_date}`;
+    releaseDate.id = "movie-release-date";
+
+    const genre = document.createElement("span");
+    genre.classList.add("genre", "f-s-mi");
+
+    if (Array.isArray(movie.genres)) {
+      genre.textContent = "Genres: " + movie.genres.map(genre => genre.name).join(", ");
+    } else {
+      genre.textContent = "Genres: N/A";
+    }
+
+    genre.id = "movie-genre";
+
+    imgDiv.appendChild(img);
+    movieInfoContainer.appendChild(title);
+    movieInfoContainer.appendChild(releaseDate);
+    movieInfoContainer.appendChild(genre);
+
+    movieCard.appendChild(imgDiv);
+    movieCard.appendChild(movieInfoContainer);
+
+    return movieCard;
   }
 
   // Handle movie card click event
@@ -88,23 +94,28 @@ document.addEventListener("DOMContentLoaded", function () {
     // Log the movieId value to check if it's set correctly
     console.log("Clicked Movie ID:", movieId);
 
-    // Call the fetchData function to retrieve movie details using the extracted movieId
-    fetchData(movieId);
+    // Call the function to display similar movies
+    displaySimilarMovies(movieId);
   }
 
-  // Function to fetch detailed information for a movie
-  async function fetchData(movieId) {
-    try {
-      // Fetch detailed information for the selected movie
-      const response = await fetch(`${movieDetailsEndpoint}/${movieId}?api_key=${apiKey}&language=en-US`);
-      const detailedMovie = await response.json();
+  // Function to fetch and display similar movies
+  function displaySimilarMovies(movieId) {
+    const similarMoviesEndpoint = `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${apiKey}&language=en-US&page=1`;
 
-      // Log the detailed movie information
-      console.log("Detailed Movie Information:", detailedMovie);
+    fetch(similarMoviesEndpoint)
+      .then(response => response.json())
+      .then(data => {
+        // Display similar movies in the UI
+        const similarMoviesContainer = document.querySelector(".similar-movies");
+        similarMoviesContainer.innerHTML = ""; // Clear previous content
 
-      // You can now use the detailedMovie data to update the UI or perform other actions
-    } catch (error) {
-      console.error("Error fetching detailed movie information:", error);
-    }
+        const similarMoviesToDisplay = data.results.slice(0, 4);
+
+        similarMoviesToDisplay.forEach(similarMovie => {
+          const similarMovieCard = createMovieCard(similarMovie);
+          similarMoviesContainer.appendChild(similarMovieCard);
+        });
+      })
+      .catch(error => console.error("Error fetching similar movies:", error));
   }
 });
